afd80b6001e2a6145ba3be5d69dc137649592e60
917trancanaomi <naomi.tranca@stud.ubbcluj.ro>
2024-01-08 18:26:54 +0200

    changes

--- cardiowave/lib/screens/result_screen.dart
+++ cardiowave/lib/screens/result_screen.dart
@@ -3,6 +3,8 @@ import 'package:cardiowave/backend/ai_script_management.dart';
 import 'package:cardiowave/backend/file_management.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:cardiowave/screens/form_page.dart';
+
 
 import '../widgets/custom_button.dart';
 
@@ -14,7 +16,7 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   FileManager fileManager = FileManager();
   AIScript aiScript = AIScript();
-  File? _resultImage;
+  List<File?> _resultImages = [null, null];
   String resultDistance = "-1";
   static final _log = Logger();
 
@@ -42,76 +44,142 @@ class _ResultScreenState extends State<ResultScreen> {
           return Text('Error retrieving result');
         } else {
           resultDistance = snapshot.data!;
-          return FutureBuilder(
-              future: fileManager.retrieveImage(),
-              builder: (context, snapshot_image) {
-                if (snapshot_image.connectionState == ConnectionState.waiting) {
-                  // Show circular progress indicator
-                  return Container(
-                    color: Colors.white,
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                } else if (snapshot_image.hasError) {
-                  // Handle error
-                  _log.e('Error: ${snapshot_image.error}');
-                  return Text('Error retrieving result');
-                } else {
-                  _resultImage = fileManager.file;
-                  return Scaffold(
-                    body: Stack(
-                      children: [
-                        Container(
-                          color: Colors.white,
-                        ),
-                        Positioned(
-                            left: 30,
-                            top: 30,
-                            bottom: 30,
-                            width: MediaQuery.of(context).size.width * 0.7,
-                            child: GestureDetector(
-                              child: _buildResultImage(),
-                            )),
-                        Positioned(
-                          top: 10,
-                          right: 10,
-                          child: Icon(
-                            Icons.image,
-                            size: 30,
+          return FutureBuilder(future: Future.wait(
+            _resultImages.map((image) {
+              return fileManager.retrieveImage();
+            }),
+          ), builder: (context, snapshotImages) {
+            if (snapshotImages.connectionState == ConnectionState.waiting) {
+              // Show circular progress indicator
+              return Container(
+                color: Colors.white,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            } else if (snapshotImages.hasError) {
+              // Handle error
+              _log.e('Error: ${snapshotImages.error}');
+              return Text('Error retrieving result');
+            } else {
+              for (int i = 0; i < snapshotImages.data!.length; i++) {
+                _resultImages[i] = fileManager.file;
+              }
+
+              return Scaffold(
+                body: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    AppBar(
+                      backgroundColor: Colors.white,
+                      elevation: 0,
+                      flexibleSpace: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: const Image(
+                              image: AssetImage('assets/images/logo.png'),
+                              fit: BoxFit.cover,
+                              width: 50,
+                              height: 50,
+                            ),
                           ),
+                          const SizedBox(width: 16),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: _buildResultImage(_resultImages[0]),
+                            ),
+                            SizedBox(width: 32.0),
+                            Expanded(
+                              child: _buildResultImage(_resultImages[1]),
+                            ),
+                          ],
                         ),
-                        Positioned(
-                          right: 50,
-                          top: MediaQuery.of(context).size.height * 0.4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                    ),
+                    // Positioned(
+                    //   top: 0,
+                    //   right: 0,
+                    //   child: Padding(
+                    //     padding: const EdgeInsets.all(16.0),
+                    //     child: Image(
+                    //       image: AssetImage('assets/images/logo.png'),
+                    //       fit: BoxFit.cover,
+                    //       width: 50,
+                    //       height: 50,
+                    //     ),
+                    //   ),
+                    // ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 42.0, left: 250.0, right: 430.0), // Adjust as needed
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "REZULTAT",
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 16.0),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              Center(child: Text(resultDistance)),
                               _buildCustomButton(
-                                  "REÎNCEARCĂ DIAGNOSTIC", () {}),
+                                "GENEREAZĂ RAPORT",
+                                    () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => FormPage(),
+                                    ),
+                                  );
+                                }
+                              ),
+                              SizedBox(height: 16.0),
+                              _buildCustomButton("ANULEAZĂ", () {}),
                             ],
                           ),
-                        ),
-                        Positioned(
-                          right: 50,
-                          bottom: 30,
-                          child: _buildCustomButton("ANULEAZĂ", () {}),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  );
-                }
-              });
+                  ],
+                ),
+              );
+            }
+          });
         }
       },
     );
   }
 
-  Widget _buildResultImage() {
+  Widget _buildResultImage(File? image) {
     return Positioned.fill(
       child: Image.file(
-        _resultImage!,
+        image!,
         fit: BoxFit.cover,
       ),
     );
---
+++ cardiowave/lib/screens/splash_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:cardiowave/screens/drag_file_screeen.dart';
+import 'package:cardiowave/screens/form_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 2),
+          () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DragFileScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.asset(
+        "assets/images/loading_page.png", // Replace with your splash image
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+    );
+  }
+}
